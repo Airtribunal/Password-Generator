d8202378e349ff63be2a8b815a3439a80c7ecc4a
Airtribunal <airtribunal@gmail.com>
2022-08-07 23:40:23 +0300

    styled-completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [password, setPassword] = useState(["!2jarjaiof747:d."])
+  const [isShowedPassword, setIsShowedPassword] = useState(false)
+
+  function generatePassword() {
+    setIsShowedPassword(prevState => !prevState)
+  }
+
   return (
     <section id="generator">
       <div className="container">
         <div className="generator__content">
           <h1 className="title">Create <br /><strong>random password</strong></h1>
           <div className="holder">
-            <input type="text"  className="input" readOnly/>
+            <input type="text" value={isShowedPassword ? password : ""} className="input" readOnly/>
             <button id="copy-btn" className="button">Copy</button>
           </div>
-          <button id="generator-btn" className="button">Generate password</button>
+          <button onClick={generatePassword} id="generator-btn" className="button extra">Generate password</button>
         </div>
       </div>
     </section>
